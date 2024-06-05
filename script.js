70c24aa7ce6e63c6c0172df53be0b6d8664a228e
Filip Korbut <109241182+filipkorbut@users.noreply.github.com>
2024-06-05 12:44:05 +0200

    test

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+function navigate(event, sectionId) {
+    event.preventDefault();
+    const section = document.getElementById(sectionId.toLowerCase()); // Użyj toLowerCase(), aby dopasować identyfikator sekcji
+    section.scrollIntoView({ behavior: 'smooth' });
+    history.pushState(null, '', '#' + sectionId.toLowerCase()); // Użyj # przed identyfikatorem sekcji
+}
+
+window.addEventListener('popstate', function() {
+    const hash = window.location.hash.slice(1); // Usuń pierwszy znak (#) z identyfikatora sekcji
+    if (hash) {
+        const section = document.getElementById(hash); // Nie potrzebujesz zmieniać wielkości liter tutaj
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+
+        }
+    }
+)
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el) => observer.observe(el));
