d395f42bc373c28ec826be05d2d5b6c570c16d41
Filip Korbut <109241182+filipkorbut@users.noreply.github.com>
2024-06-05 13:27:41 +0200

    test

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const langElements = document.querySelectorAll(".lang");
-
-    langElements.forEach(function(langElement) {
-        langElement.addEventListener("click", function() {
-            const lang = this.getAttribute("data-lang");
-            window.location.href = lang + ".html";
-        });
+  
+    langElements.forEach(function (langElement) {
+      langElement.addEventListener("click", function () {
+        const lang = this.getAttribute("data-lang");
+        window.location.href = lang + ".html";
+      });
     });
-});
-function navigate(event, sectionId) {
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+  
+    const hiddenElements = document.querySelectorAll(".hidden");
+  
+    hiddenElements.forEach((el) => observer.observe(el));
+  });
+  
+  function navigate(event, sectionId) {
     event.preventDefault();
-    const section = document.getElementById(sectionId.toLowerCase()); // Użyj toLowerCase(), aby dopasować identyfikator sekcji
-    section.scrollIntoView({ behavior: 'smooth' });
-    history.pushState(null, '', '#' + sectionId.toLowerCase()); // Użyj # przed identyfikatorem sekcji
-}
-
-window.addEventListener('popstate', function() {
-    const hash = window.location.hash.slice(1); // Usuń pierwszy znak (#) z identyfikatora sekcji
-    if (hash) {
-        const section = document.getElementById(hash); // Nie potrzebujesz zmieniać wielkości liter tutaj
-        section.scrollIntoView({ behavior: 'smooth' });
+    const section = document.getElementById(sectionId.toLowerCase()); 
+    section.scrollIntoView({ behavior: "smooth" });
+    history.pushState(null, "", "#" + sectionId.toLowerCase()); 
+  
+    // Sprawdzenie, czy sectionId to 'about', jeśli tak, usuń ten fragment z adresu URL
+    if (sectionId.toLowerCase() === 'about') {
+      history.replaceState(null, "", window.location.pathname); // Usunięcie fragmentu z adresu URL
     }
-});
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        }
-        else{
-            entry.target.classList.remove('show');
-
-        }
-    }
-)
-});
-
-const hiddenElements = document.querySelectorAll(".hidden");
-
-hiddenElements.forEach((el) => observer.observe(el));
+  }
+  
