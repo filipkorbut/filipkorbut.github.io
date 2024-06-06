28e64b89a7f8905008ce365db61837b1223e9d85
Filip Korbut <109241182+filipkorbut@users.noreply.github.com>
2024-06-06 23:16:32 +0200

    no footer

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const langElements = document.querySelectorAll(".lang");
-  
-    langElements.forEach(function (langElement) {
+  const langElements = document.querySelectorAll(".lang");
+
+  langElements.forEach(function (langElement) {
       langElement.addEventListener("click", function () {
-        const lang = this.getAttribute("data-lang");
-        window.location.href = lang + ".html";
+          const lang = this.getAttribute("data-lang");
+          window.location.href = lang + ".html";
       });
-    });
-  
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-        } else {
-          entry.target.classList.remove("show");
-        }
-      });
-    });
-  
-    const hiddenElements = document.querySelectorAll(".hidden");
-  
-    hiddenElements.forEach((el) => observer.observe(el));
   });
-  
-  function navigate(event, sectionId) {
-    event.preventDefault();
-    const section = document.getElementById(sectionId.toLowerCase()); 
-    section.scrollIntoView({ behavior: "smooth" });
-  
-    // Usuń cały anchor z adresu URL
-    history.pushState(null, "", window.location.pathname);
-  }
-  
+
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+              const delay = entry.target.getAttribute('data-delay') || 0;
+              entry.target.style.animationDelay = `${delay}s`;
+              entry.target.classList.add("show");
+              if (entry.target.classList.contains("hidden-jump")) {
+                  void entry.target.offsetWidth;
+                  entry.target.classList.add("animate");
+              }
+              if (entry.target.classList.contains("slide-left")) {
+                  void entry.target.offsetWidth;
+                  entry.target.classList.add("slide-in");
+              }
+          } else {
+              entry.target.classList.remove("show");
+              if (entry.target.classList.contains("hidden-jump")) {
+                  entry.target.classList.remove("animate");
+              }
+              if (entry.target.classList.contains("slide-left")) {
+                  entry.target.classList.remove("slide-in");
+              }
+          }
+      });
+  });
+
+  const hiddenElements = document.querySelectorAll(".hidden");
+  const hiddenJump = document.querySelectorAll(".hidden-jump");
+  const slideLeft = document.querySelectorAll(".slide-left");
+
+  hiddenElements.forEach((el) => observer.observe(el));
+  hiddenJump.forEach((el) => observer.observe(el));
+  slideLeft.forEach((el) => observer.observe(el));
+});
